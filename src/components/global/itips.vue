6dfb24c8985a82b8da4bc04eb98cdbb42a03beca
zhangchao111 <244167604@qq.com>
2021-05-27 17:57:38 +0800

    开发发现中得菜单

---
+++ src/components/global/itips.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="itipTitle">
+        <span class="i-t">444</span>
+        <span class="i-r">
+            <slot name="default"></slot>
+<!--            <slot></slot>-->
+        </span>
+    </div>
+</template>
+<script lang="ts">
+    import {
+        defineComponent,
+        ref,
+        onBeforeMount,
+        onMounted,
+        onBeforeUpdate,
+        onBeforeUnmount,
+        onUnmounted,
+        watch,
+        watchEffect
+    } from 'vue'
+
+    export default defineComponent({
+        components: {},
+        setup() {
+
+            return {}
+        },
+        methods: {}
+    })
+</script>
+<style>
+
+</style>
--- src/views/find/index.vue
+++ src/views/find/index.vue
@@ -9,6 +9,26 @@
                 />
             </van-swipe-item>
         </van-swipe>
+        <div class="find-Menu">
+            <div class="warp" :style="{'width':menuList.length*1.57+'rem'}">
+                    <div class="menu-item" v-for="(item,index) in menuList" :key="index">
+                        <div class="menu-iconwarp">
+                            <span :class="[item.src,'icon','m_iconfont']"></span>
+                        </div>
+                        <div class="menu-title">{{item.name}}</div>
+                    </div>
+            </div>
+        </div>
+
+        <div class="find-tjgd">
+            <itips>
+                <template v-slot>111</template>
+<!--                <template v-slot:default>111</template>-->
+            </itips>
+        </div>
+
+
+
     </div>
 
 
@@ -17,6 +37,7 @@
     import {
         defineComponent,
         ref,
+        reactive,
         onBeforeMount,
         onMounted,
         onBeforeUpdate,
@@ -29,11 +50,22 @@
     export default defineComponent({
         components: {},
         setup() {
-            const images = [
+            const images =reactive([
                 'https://img.yzcdn.cn/vant/apple-1.jpg',
                 'https://img.yzcdn.cn/vant/apple-2.jpg',
-            ];
-            return {images}
+            ]);
+            const menuList=reactive([
+                {src:'m_iconmeirigengxin',name:'每日推荐'},
+                {src:'m_iconFM_weixuanze',name:'私人FM'},
+                {src:'m_icongedan',name:'歌单'},
+                {src:'m_iconpaihangbang',name:'排行榜'},
+                {src:'m_iconhuiyuan',name:'直播'},
+                {src:'m_iconzhuanji',name:'数字专辑'},
+                {src:'m_iconhuiyuan',name:'专注冥想'},
+                {src:'m_iconyinle1',name:'歌房'},
+                {src:'m_iconhanhan-01-01',name:'游戏专区'},
+            ])
+            return {images,menuList}
         },
         methods: {}
     })
