a54aa8494b26e30edb31dabe974cb610ae2c9957
e <244167604@qq.com>
2021-06-02 23:23:57 +0800

    update fing gc-gxq

--- src/views/video/index.vue
+++ src/views/video/index.vue
@@ -25,11 +25,11 @@
                                 </van-image>
                             </div>
                             <div class="right">
-                                <div class="titles">网易云音乐  分享单曲</div>
+                                <div class="r-titles">网易云音乐  <span>分享单曲</span></div>
                                 <div class="time">46分钟前</div>
                                 <div class="description">
-                                    <p>#云村夜话#</p>
-                                    <p>听到诗，你第一时间能想到那首诗或者哪位诗人</p>
+                                    <p class="p-ycyh">#云村夜话#</p>
+                                    <p class="p-text">听到诗，你第一时间能想到那首诗或者哪位诗人</p>
                                     <div class="img">
                                         <van-image
                                                 fit="cover"
@@ -59,26 +59,56 @@
                                             <p>清平调（独唱版）</p>
                                             <p>王菲</p>
                                         </div>
-                                        <div class="ins">
-                                            网易云音乐的云圈
-                                        </div>
-                                        <div class="rate">
-                                            <div class="left"><span class="icon m_iconfont m_iconxunhuan1
-"></span>1</div>
-                                            <div class="middle1"><span class="icon m_iconfont m_iconweibiaoti-
-"></span>78</div>
-                                            <div class="middle2"><span class="icon m_iconfont m_iconzan
-"></span>103</div>
-                                            <div class="right"><span class="icon m_iconfont m_iconziyuan
-"></span></div>
-                                        </div>
                                     </div>
+                                    <div class="ins">
+                                        <span>网易云音乐的云圈</span>
+                                    </div>
+                                    <div class="rate">
+                                            <div class="pub r-left"><span class="icon m_iconfont m_iconxunhuan1
+"></span> 1</div>
+                                            <div class="pub middle1"><span class="icon m_iconfont m_iconweibiaoti-
+"></span> 78</div>
+                                            <div class="pub middle2"><span class="icon m_iconfont m_iconzan
+"></span> 103</div>
+                                            <div class="pub r-right"><span class="icon m_iconfont m_iconziyuan
+"></span></div>
+                                     </div>
+
                                 </div>
                             </div>
                         </div>
                     </div>
+                    <div class="loadmores van-hairline--bottom">
+                        展开更多历史动态
+                        <van-icon name="arrow-down" />
+<!--                        <van-icon name="arrow-up" />-->
+                    </div>
+
 
                 </div>
+
+                <div class="gxqr">
+                        <itips>
+                            <template #title>你可能感兴趣的人</template>
+                            <template #more>更多 <van-icon name="arrow" /></template>
+                        </itips>
+                    <div class="gxqr-warp">
+                        <swiper
+                                :spaceBetween="10"
+                                :loop="false"
+                                :slidesPerView="3"
+                                class="swiper-container"
+                        >
+                            <swiper-slide class="gxqr-item" v-for="i in 8" :key="i"  >
+                                dfgdfgdfg
+                            </swiper-slide>
+                        </swiper>
+                    </div>
+
+
+                </div>
+
+
             </van-tab>
             <van-tab title="推荐" name="tj">
                 <van-tabs v-model:active="activeItem" swipeable>
@@ -138,9 +168,15 @@
         watch,
         watchEffect
     } from 'vue'
+    import { Swiper, SwiperSlide } from 'swiper/vue';
+    import 'swiper/swiper.min.css';
+    import 'swiper/components/pagination/pagination.less';
     import {useRouter } from 'vue-router';
     export default defineComponent({
-        components: {},
+        components: {
+            Swiper,
+            SwiperSlide,
+        },
         setup() {
             const router = useRouter();
             const active=ref("gz");
